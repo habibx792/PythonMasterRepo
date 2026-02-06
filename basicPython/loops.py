2498d1c26e26641a882eed3025fcd1f6cd4d8c8c
Ghulam Habib <ghulamhabib.x@gamil.com>
2026-02-06 15:14:30 +0500

    learing python by dave grave

---
+++ basicPython/loops.py
@@ -0,0 +1,14 @@
+for i in range(1,20,3):
+    print(i)
+listy=[1,2,3,4,5,6,]
+
+def ui():
+    print("========================")
+
+for i in listy:
+    print(i)
+i=0
+ui()
+while i!=10:
+    print(i)
+    i+=1
