4ce2034d4cf40928a1df851ed608ae73d13b413c
Ghulam Habib <ghulamhabib.x@gamil.com>
2026-02-08 01:59:50 +0500

    done

---
+++ basicPython/oop/constructor.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(this,name,age):
+        this.name=name
+        this.age=age
+    def print(this):
+        print(this.name)
+        print(this.age)
+c=Car("Honda Cive",12)
+c.print()
